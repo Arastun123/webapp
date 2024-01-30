382a60e0d87717e166d9a544f62ceb0239ae40c6
Arastun123 <ekbr03@gmail.com>
2024-01-30 13:49:14 +0400

    Adding Nomenklatura to db, type of Kontragent

--- app.js
+++ app.js
@@ -27,23 +27,20 @@ app.get('/', (req, res) => { res.sendFile(__dirname + '/index.htm') });
 
 app.post('/submit/nomenklatura', (req, res) => {
     const formData = req.body;
-
     console.log(formData);
-    // db.query('INSERT INTO nomenklatura SET ?', formData, (err, result) => {
-    //     if (err) {
-    //         console.error(err);
-    //         res.status(500).send('Internal Server Error');
-    //     } else {
-    //         console.log(result);
-    //         res.send('Form submitted successfully for Nomenklatura!');
-    //     }
-    // });
+    db.query('INSERT INTO nomenklatura SET ?', formData, (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.json({ success: true });
+        }
+    });
 });
 
 
 app.post('/submit/kontragent', (req, res) => {
     const formData = req.body;
-
     db.query('INSERT INTO kontragent SET ?', formData, (err, result) => {
         if (err) {
             console.error(err);
